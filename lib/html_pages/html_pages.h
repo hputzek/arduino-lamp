5ce63ffe933cfbba6bc6e18af4467a0a7f60eb4a
Hendrik Putzek <hendrik@putzek.com>
2017-05-16 22:40:14 +0200

    cleanup and more stability

--- lib/html_pages/html_pages.h
+++
@@ -1,9 +0,0 @@
-//
-//  HTML PAGES
-//
-const char updateHTTP[] PROGMEM =  R"=====(
-<form method='POST' action='/update' enctype='multipart/form-data'>
-<input type='file' name='update'>
-<input type='submit' value='Update'>
-</form>
-)=====";
--- src/lampe.cpp
+++ src/lampe.cpp
@@ -1,13 +1,12 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 #include <PubSubClient.h>
 #include <hw_timer.h>
 #include "Dimmer.h"
 #include <Fader.h>
 #include <ArduinoJson.h>
 #include "FS.h"
+#include "settings.h"
 
 // declarations
 void mqttCallback(char* topic, byte* payload, unsigned int length);
@@ -28,8 +27,8 @@ bool state = false;
 // lamp setup
 #define LAMP_NUM 4
 
-const char* ssid = "***REMOVED***";
-const char* password = "***REMOVED***";
+// const char* ssid = "my-ssid";
+// const char* password = "password";
 
 // MQTT Settings
 //const char* mqtt_server = "m2m.eclipse.org";
@@ -252,8 +251,8 @@ void setup() {
       delay(500);
       yield();
     }
+
   yield();
-  noInterrupts();
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -261,47 +260,6 @@ void setup() {
     yield();
     ESP.restart();
   }
-  interrupts();
-
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  // ArduinoOTA.setHostname("myesp8266");
-
-  // No authentication by default
-  // ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-  ArduinoOTA.onStart([]() {
-   String type;
-   if (ArduinoOTA.getCommand() == U_FLASH)
-     type = "sketch";
-   else // U_SPIFFS
-     type = "filesystem";
-
-   // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-   Serial.println("Start updating " + type);
- });
- ArduinoOTA.onEnd([]() {
-   Serial.println("\nEnd");
- });
- ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-   Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
- });
- ArduinoOTA.onError([](ota_error_t error) {
-   Serial.printf("Error[%u]: ", error);
-   if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-   else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-   else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-   else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-   else if (error == OTA_END_ERROR) Serial.println("End Failed");
- });
-
-  //ArduinoOTA.begin();
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -309,7 +267,6 @@ void setup() {
 }
 
 void loop() {
-  //ArduinoOTA.handle();
   yield();
   setupMQTTClient();
   mqttClient.loop();
