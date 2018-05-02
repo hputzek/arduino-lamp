7f9ab11ffbc94c5f3a8ab3830342618a535a44e0
Hendrik Putzek <hendrik@putzek.com>
2018-05-02 23:39:39 +0200

    Began rewrite of ESP8266 code

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -1,399 +1,286 @@
-#include <ESP8266WiFi.h>
-#include <WiFiUdp.h>
-#include <PubSubClient.h>
-#include <hw_timer.h>
-#include <Dimmer.h>
-#include <Fader.h>
-#include <ArduinoJson.h>
-#include <FS.h>
-#include <main.h>
-#include <settings.h>
+#include "FS.h"          //this needs to be first, or it all crashes and burns...
+#include "ESP8266WiFi.h" //https://github.com/esp8266/Arduino
+#include "DNSServer.h"
+#include "ESP8266WebServer.h"
+#include "WiFiManager.h" //https://github.com/tzapu/WiFiManager
+#include "AsyncMqttClient.h"
+#include "ArduinoJson.h"
+#include "Arduino.h"
+#include "main.h"
 
+char mqtt_server[40];
+char mqtt_port[6] = "8080";
+char mqtt_device_address[64] = "livingRoom";
 
-bool loadSettings(){
-  DynamicJsonBuffer jsonSettings;
-  File configFile = SPIFFS.open("/config.json", "r");
-    if (!configFile) {
-      File configFile = SPIFFS.open("/config.json", "w");
-      configFile.print("{}");
-      Serial.println("Failed to open config file");
-      return false;
-    }
+// state
+TOPICS topics;
+STATE state;
 
-    size_t size = configFile.size();
-    if (size > 1024) {
-      Serial.println("Config file size is too large");
-      return false;
-    }
-    // Allocate a buffer to store contents of the file.
-    std::unique_ptr<char[]> buf(new char[size]);
+//flag for saving data
+bool shouldSaveConfig = false;
 
-    // We don't use String here because ArduinoJson library requires the input
-    // buffer to be mutable. If you don't use ArduinoJson, you may as well
-    // use configFile.readString instead.
-    configFile.readBytes(buf.get(), size);
+AsyncMqttClient mqttClient;
 
-    configFile.close();
-
-    JsonObject& settings = jsonSettings.parseObject(buf.get());
-
-
-    if (!settings.success()) {
-      Serial.println("Failed to parse config file");
-      return false;
-    }
-    if (preset == 0) {
-      preset = settings["activePreset"];
-    }
-  state = settings["state"];
-  brightness = settings["presets"][String(preset)]["brightness"];
-  fade = settings["presets"][String(preset)]["fade"];
-  spread = settings["presets"][String(preset)]["spread"];
-  return true;
-}
-
-bool saveSettings() {
-  DynamicJsonBuffer jsonSettings;
-  File configFile = SPIFFS.open("/config.json", "r");
-    if (!configFile) {
-      Serial.println("Failed to open config file");
-      return false;
-    }
-
-    size_t size = configFile.size();
-    if (size > 1024) {
-      Serial.println("Config file size is too large");
-      return false;
-    }
-    // Allocate a buffer to store contents of the file.
-    std::unique_ptr<char[]> buf(new char[size]);
-
-    // We don't use String here because ArduinoJson library requires the input
-    // buffer to be mutable. If you don't use ArduinoJson, you may as well
-    // use configFile.readString instead.
-    configFile.readBytes(buf.get(), size);
-
-    configFile.close();
-
-  JsonObject& settings = jsonSettings.parseObject(buf.get());
-  settings["activePreset"] = preset;
-  settings["state"] = state;
-  if(!settings["presets"]) {
-     settings.createNestedObject("presets");
-  }
-  JsonObject& presets = settings["presets"];
-  JsonObject& currentPreset =  presets.createNestedObject(String(preset));
-
-  settings["presets"][String(preset)]["brightness"] = brightness;
-  settings["presets"][String(preset)]["fade"] = fade;
-  settings["presets"][String(preset)]["spread"] = spread;
-  File configFileSave = SPIFFS.open("/config.json", "w");
-
-  if (!configFileSave) {
-    Serial.println("Failed to open config file for writing");
-    return false;
-  }
-  settings.prettyPrintTo(Serial);
-  settings.printTo(configFileSave);
-
-  configFile.close();
-  return true;
-}
-
-void ICACHE_RAM_ATTR publishState(){
-    if (mqttClient.connected()) {
-      hw_timer_stop();
-      mqttClient.publish(mqtt_brightness_topic, String(brightness).c_str());
-      mqttClient.publish(mqtt_fade_topic, fade ? "ON" : "OFF");
-      mqttClient.publish(mqtt_spread_topic, String(spread).c_str());
-      //Dimmer *dimmer =
-      dimmers[0].restart();
-    }
-}
-
-void lampCallback(byte id, uint8_t brightness){
-      yield();
-      dimmers[id-1].set(brightness);
-      yield();
-}
-
-void getLampsBrightness(int *brightnessArray, byte lampCount, int lowerBoundary, int upperBoundary,int spread, int brightness) {
-    //spread /= 2;
-    int loopCount = lampCount;
-
-    if (lampCount % 2 == 1) {
-      loopCount--;
-    }
-
-    for (int i = 0; i < loopCount; i += 2) {
-      int randomValue = random(spread / 2, spread);
-      int upperOverflow = (brightness + randomValue) - upperBoundary;
-      int lowerOverflow = (brightness -  randomValue) + lowerBoundary;
-
-      if(upperOverflow < 0) {
-        upperOverflow = 0;
-      }
-
-      if (lowerOverflow > 0) {
-        lowerOverflow = 0;
-      }
-      brightnessArray[i] = min(brightness + randomValue, upperBoundary) + lowerOverflow;
-      brightnessArray[i + 1] = max(brightness - randomValue, lowerBoundary) + upperOverflow ;
-    }
-
-    bubbleUnsort(brightnessArray,lampCount);
-}
-
-byte getMappedBrightness() {
-  return map(brightness, 1, 100, 1, 255);
-}
-
-void updateLamps() {
-  if(fade) {
-    int duration = random(1000, 3000);
-    if (lamps[0].is_fading() == false) {
-      getLampsBrightness(brightnessArray, LAMP_NUM, 1, 255, spread, getMappedBrightness());
-      Serial.println(brightnessArray[0]);
-      Serial.println(brightnessArray[1]);
-      Serial.println((brightnessArray[0] + brightnessArray[1]) /2 );
-      Serial.println("---");
-
-      for (byte i = 0; i < LAMP_NUM; i++) {
-        lamps[i].fade(brightnessArray[i], duration);
-        lamps[i].update();
-      }
-    } else {
-      for (byte i = 0; i < LAMP_NUM; i++) {
-        lamps[i].update();
-      }
-    }
-  } else {
-    for (byte i = 0; i < LAMP_NUM; i++) {
-      lamps[i].set_value(getMappedBrightness());
-      lamps[i].update();
-    }
-  }
-}
-
-
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting");
-
-  Serial.println("Mounting FS...");
-
-    if (!SPIFFS.begin()) {
-      Serial.println("Failed to mount file system");
-      return;
-    }
-
-    if (!loadSettings()) {
-      Serial.println("Failed to load config");
-    } else {
-      Serial.println("Config loaded");
-    }
-
-    randomSeed(micros());
-    for (byte i = 0; i < LAMP_NUM; i++) {
-      dimmers[i].begin(brightness,true);
-      updateLamps();
-      delay(500);
-      yield();
-    }
-
-  yield();
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    ESP.restart();
-  }
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-void loop() {
-  yield();
-  setupMQTTClient();
-  mqttClient.loop();
-  yield();
-  updateLamps();
-  yield();
-}
-
-void bubbleSort(float A[],int len) {
-  unsigned long newn;
-  unsigned long n=len;
-  float temp=0.0;
-  do {
-    newn=1;
-    for(int p=1;p<len;p++){
-      if(A[p-1]>A[p]){
-        temp=A[p];           //swap places in array
-        A[p]=A[p-1];
-        A[p-1]=temp;
-        newn=p;
-      } //end if
-    } //end for
-    n=newn;
-  } while(n>1);
-}
-
-void bubbleUnsort(int *list, int length)
+//callback notifying us of the need to save config
+void saveConfigCallback()
 {
- for (int a=length-1; a>0; a--)
- {
-   int r = random(a+1);
-   //int r = rand_range(a+1);
-   if (r != a)
-   {
-     int temp = list[a];
-     list[a] = list[r];
-     list[r] = temp;
-   }
- }
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
 }
 
+void onMqttConnect(bool sessionPresent)
+{
+  mqttClient.subscribe(getTopic(topics.mqtt_single_mode_topic), 1);
+  mqttClient.subscribe(getTopic(topics.mqtt_state_topic), 1);
+  mqttClient.subscribe(getTopic(topics.mqtt_brightness_topic), 1);
+  mqttClient.subscribe(getTopic(topics.mqtt_fade_topic), 1);
+  mqttClient.subscribe(getTopic(topics.mqtt_fade_upper_boundary_topic), 1);
+  mqttClient.subscribe(getTopic(topics.mqtt_fade_lower_boundary_topic), 1);
+  mqttClient.subscribe(getTopic(topics.mqtt_spread_topic), 1);
+  // handle status
+  mqttClient.publish(getTopic(topics.mqtt_status_topic), 2, true, "online");
+  mqttClient.setWill(getTopic(topics.mqtt_status_topic), 2, true, "offline");
+}
 
-void setupMQTTClient() {
-   while (!mqttClient.connected()) {
-      yield();
-      int connectResult;
-      // Create a random client ID
-         String clientId = "ESP8266Client-";
-         clientId += String(random(0xffff), HEX);
-         // Attempt to connect
-      if (mqtt_server != "") {
-        Serial.print("MQTT client: ");
-        mqttClient.setServer(mqtt_server, mqtt_port);
-        mqttClient.setCallback(mqttCallback);
-        if (mqtt_user == "") {
-          connectResult = mqttClient.connect(clientId.c_str());
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
+{
+  if (getTopic(topics.mqtt_single_mode_topic) == topic)
+  {
+    state.singleMode = (bool)payload;
+  };
+  if (getTopic(topics.mqtt_state_topic) == topic)
+  {
+    state.state = (bool)payload;
+  };
+  if (getTopic(topics.mqtt_brightness_topic) == topic)
+  {
+    if (state.singleMode)
+    {
+      char **brightness = NULL;
+      int count = split(payload, ',', &brightness);
+
+    for (uint8_t i = 0; i < count; i++)
+        state.brightnessSingle[i] = atoi(brightness[i]);
+    }
+    else
+    {
+      state.brightness = constrain(atoi(payload), 0, 100);
+    }
+  };
+  if (getTopic(topics.mqtt_fade_topic) == topic)
+  {
+    state.fade = (bool)payload;
+  };
+  if (getTopic(topics.mqtt_fade_lower_boundary_topic) == topic)
+  {
+    state.fadeLowerBoundary = constrain(atoi(payload), 100, 60000);
+  };
+  if (getTopic(topics.mqtt_fade_upper_boundary_topic) == topic)
+  {
+    state.fadeUpperBoundary = constrain(atoi(payload), state.fadeLowerBoundary, 60000);
+  };
+  if (getTopic(topics.mqtt_spread_topic) == topic)
+  {
+    state.spread = constrain(atoi(payload), 0, 100);
+  };
+
+  publishState();
+};
+
+void publishState()
+{
+  const size_t bufferSize = JSON_ARRAY_SIZE(4) + JSON_OBJECT_SIZE(6);
+  DynamicJsonBuffer jsonBuffer(bufferSize);
+
+  JsonObject &root = jsonBuffer.createObject();
+  root["singleMode"] = state.singleMode;
+  if (state.singleMode)
+  {
+    JsonArray &brightness = root.createNestedArray("brightness");
+    brightness.add(state.brightnessSingle[0]);
+    brightness.add(state.brightnessSingle[1]);
+    brightness.add(state.brightnessSingle[2]);
+    brightness.add(state.brightnessSingle[3]);
+  }
+  else
+  {
+    root["brightness"] = state.brightness;
+  }
+
+  root["fadeLowerBoundary"] = state.fadeLowerBoundary;
+  root["fadeUpperBoundary"] = state.fadeUpperBoundary;
+  root["spread"] = state.spread;
+  root["fade"] = state.fade;
+
+  root.printTo(Serial);
+}
+void loadConfiguration()
+{
+  //read configuration from FS json
+  if (SPIFFS.begin())
+  {
+    if (SPIFFS.exists("/config.json"))
+    {
+      //file exists, reading and loading
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile)
+      {
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject &json = jsonBuffer.parseObject(buf.get());
+        if (json.success())
+        {
+
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(mqtt_device_address, json["mqtt_device_address"]);
         }
-        else {
-          connectResult = mqttClient.connect(clientId.c_str(), mqtt_user, mqtt_password);
-        }
-
-        if (connectResult) {
-          Serial.println("Connected");
-        }
-        else {
-          Serial.print("Failed (");
-          Serial.print(mqttClient.state());
-          Serial.println(")");
-        }
-        if (mqttClient.connected()) {
-          Serial.print("MQTT topic '");
-          Serial.print(mqtt_state_topic);
-          if (mqttClient.subscribe(mqtt_state_topic)) {
-            Serial.println("': Subscribed");
-          }
-          else {
-            Serial.print("': Failed");
-          }
-
-          Serial.print("MQTT topic '");
-          Serial.print(mqtt_brightness_topic);
-          if (mqttClient.subscribe(mqtt_brightness_topic)) {
-            Serial.println("': Subscribed");
-          }
-          else {
-            Serial.print("': Failed");
-          }
-
-          Serial.print("MQTT topic '");
-          Serial.print(mqtt_fade_topic);
-          if (mqttClient.subscribe(mqtt_fade_topic)) {
-            Serial.println("': Subscribed");
-          }
-          else {
-            Serial.print("': Failed");
-          }
-
-          Serial.print("MQTT topic '");
-          Serial.print(mqtt_preset_topic);
-          if (mqttClient.subscribe(mqtt_preset_topic)) {
-            Serial.println("': Subscribed");
-          }
-          Serial.print("MQTT topic '");
-          Serial.print(mqtt_spread_topic);
-          if (mqttClient.subscribe(mqtt_spread_topic)) {
-            Serial.println("': Subscribed");
-          }
-          else {
-            Serial.print("': Failed");
-          }
-       }
+      }
     }
   }
 }
 
-void mqttCallback(char* topic, byte* payload, unsigned int length) {
-  char c_payload[length];
-  memcpy(c_payload, payload, length);
-  c_payload[length] = '\0';
+void connect()
+{
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 6);
+  WiFiManagerParameter custom_mqtt_address("address", "mqtt device address", mqtt_device_address, 64);
 
-  String s_topic = String(topic);
-  String s_payload = String(c_payload);
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
 
-  if (mqttDebug) {
-    Serial.print("MQTT in: ");
-    Serial.print(s_topic);
-    Serial.print(" = ");
-    Serial.print(s_payload);
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //set static ip
+  wifiManager.setSTAStaticIPConfig(IPAddress(10, 0, 1, 99), IPAddress(10, 0, 1, 1), IPAddress(255, 255, 255, 0));
+
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_address);
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("AutoConnectAP", "password"))
+  {
+    Serial.println("failed to connect and hit timeout");
+    delay(3000);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_device_address, custom_mqtt_address.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig)
+  {
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["mqtt_address"] = mqtt_device_address;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    json.printTo(configFile);
+    configFile.close();
+  }
+  // convert content of mqtt_server to a (hopefully) valid ip address
+  IPAddress mqttServerIp;
+  if (mqttServerIp.fromString(mqtt_server))
+  {
+    mqttClient.setServer(mqttServerIp, atoi(mqtt_port));
+    mqttClient.connect();
   }
 
-  if (s_topic == mqtt_state_topic) {
-    if (mqttDebug) { Serial.println(""); }
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onConnect(onMqttConnect);
+}
 
-    if (s_payload == "ON") {
-      state = true;
-    }
-    else if (s_payload == "OFF") {
-      state = false;
-    }
-  }
-  else if (s_topic == mqtt_fade_topic) {
-    if (mqttDebug) { Serial.println(""); }
+void setup()
+{
+  Serial.begin(115200);
+  connect();
+}
 
-    if (s_payload == "ON") {
-      fade = true;
-    }
-    else if (s_payload == "OFF") {
-      fade = false;
-    }
-  }
-  else if (s_topic == mqtt_brightness_topic) {
-    if (mqttDebug) { Serial.println(""); }
+void loop()
+{
+}
 
-    if (s_payload.toInt() != 0) {
-      brightness = (byte)s_payload.toInt();
-    }
-  }
-  else if (s_topic == mqtt_preset_topic) {
-    if (mqttDebug) { Serial.println(""); }
+char *getTopic(char *topic)
+{
+  return strcat(mqtt_device_address, (char *)topic);
+}
 
-    if (s_payload.toInt() != 0) {
-      preset = constrain(s_payload.toInt(),1,5);
-      hw_timer_stop();
-      loadSettings();
-      publishState();
-      dimmers[0].restart();
-    }
-  }
-  else if (s_topic == mqtt_spread_topic) {
-    if (mqttDebug) { Serial.println(""); }
+int split (char *str, char c, char ***arr)
+{
+    int count = 1;
+    int token_len = 1;
+    int i = 0;
+    char *p;
+    char *t;
 
-    if (s_payload.toInt() != 0) {
-      spread = constrain((byte)s_payload.toInt(), 0, 100);
+    p = str;
+    while (*p != '\0')
+    {
+        if (*p == c)
+            count++;
+        p++;
     }
-  }
-  else {
-    if (mqttDebug) { Serial.println(" [unknown message]"); }
-  }
-  hw_timer_stop();
-  saveSettings();
-  dimmers[0].restart();
+
+    *arr = (char**) malloc(sizeof(char*) * count);
+    if (*arr == NULL)
+        exit(1);
+
+    p = str;
+    while (*p != '\0')
+    {
+        if (*p == c)
+        {
+            (*arr)[i] = (char*) malloc( sizeof(char) * token_len );
+            if ((*arr)[i] == NULL)
+                exit(1);
+
+            token_len = 0;
+            i++;
+        }
+        p++;
+        token_len++;
+    }
+    (*arr)[i] = (char*) malloc( sizeof(char) * token_len );
+    if ((*arr)[i] == NULL)
+        exit(1);
+
+    i = 0;
+    p = str;
+    t = ((*arr)[i]);
+    while (*p != '\0')
+    {
+        if (*p != c && *p != '\0')
+        {
+            *t = *p;
+            t++;
+        }
+        else
+        {
+            *t = '\0';
+            i++;
+            t = ((*arr)[i]);
+        }
+        p++;
+    }
+
+    return count;
 }
--- esp8266/src/main.h
+++ esp8266/src/main.h
@@ -3,63 +3,40 @@
 
 // OTHER SETTINGS
 const byte mqttDebug = 1;
-const byte outPin1 = 14;
-const byte outPin2 = 12;
-const byte outPin3 = 13;
-const byte outPin4 = 15;
-bool fade = false;
-int preset = 1;
-byte brightness = 100;
-byte spread = 50;
-bool state = true;
-
-// lamp setup
-#define LAMP_NUM 4
 
 // MQTT Settings
-//const char* mqtt_server = "m2m.eclipse.org";
-const char* mqtt_server = "192.168.178.23";
-const int mqtt_port = 1883;
-const char* mqtt_user = "";
-const char* mqtt_password = "";
-const char* mqtt_preset_topic = "office/light1/preset";
-const char* mqtt_state_topic = "office/light1/state";
-const char* mqtt_brightness_topic = "office/light1/brightness";
-const char* mqtt_fade_topic = "office/light1/fade";
-const char* mqtt_spread_topic = "office/light1/spread";
+typedef struct
+{
+  char *mqtt_single_mode_topic = (char *)"/singleMode";
+  char *mqtt_state_topic = (char *)"/state";
+  char *mqtt_brightness_topic = (char *)"/brightness";
+  char *mqtt_fade_topic = (char *)"/fade";
+  char *mqtt_fade_lower_boundary_topic = (char *)"/fade/lowerBoundary";
+  char *mqtt_fade_upper_boundary_topic = (char *)"/fade/upperBoundary";
+  char *mqtt_spread_topic = (char *)"/spread";
+  char *mqtt_status_topic = (char *)"/status";
+} TOPICS;
 
-WiFiClient espClient;
-PubSubClient mqttClient(espClient);
-
-int brightnessArray[LAMP_NUM];
+typedef struct
+{
+  bool state = true;
+  bool singleMode = false;
+  uint8_t brightnessSingle[4] = {5, 10, 15, 20};
+  uint8_t brightness = 15;
+  int fadeLowerBoundary = 1000;
+  int fadeUpperBoundary = 6000;
+  uint8_t spread = 10;
+  bool fade = false;
+} STATE;
 
 // declarations
 
-void setupMQTTClient();
-void mqttCallback(char* topic, byte* payload, unsigned int length);
-void lampCallback(byte id, uint8_t brightness);
-void getLampsBrightness(int *brightnessArray, byte lampCount, int loverBoundary, int upperBoundary, int spread, int brightness);
-void updateLamps();
-void bubbleSort(float A[],int len);
-void bubbleUnsort(int *list, int length);
-bool saveSettings();
-bool loadSettings();
+void saveConfigCallback();
+void connect();
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+void onMqttConnect(bool);
+char *getTopic(char *topic);
+int split (char *str, char c, char ***arr);
 void publishState();
-byte getMappedBrightness();
-const char* getCharFromByte(byte);
-
-Fader lamps[LAMP_NUM] = {
-  Fader(1,lampCallback),
-  Fader(2,lampCallback),
-  Fader(3,lampCallback),
-  Fader(4,lampCallback)
-};
-
-Dimmer dimmers[LAMP_NUM] = {
-  Dimmer(outPin1, 0),
-  Dimmer(outPin2, 0),
-  Dimmer(outPin3, 0),
-  Dimmer(outPin4, 0),
-};
 
 #endif // MAIN_H
