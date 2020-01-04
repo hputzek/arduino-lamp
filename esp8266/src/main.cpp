6a1209ccaea47cbc0b6a91134e8a4ef8bc0f0701
Hendrik Putzek <hendrik@putzek.com>
2020-01-04 17:35:43 +0100

    add second ws event handler

--- esp8266/src/main.cpp
+++ esp8266/src/main.cpp
@@ -15,11 +15,49 @@ TOPICS topics;
 STATE state;
 
 AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
-AsyncEventSource events("/events");
+AsyncWebSocket wSet("/set");
+AsyncWebSocket wSave("/save");
 DNSServer dns;
 
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+void onSet(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+  if(type == WS_EVT_CONNECT){
+    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+    //client->printf("Hello Client %u :)", client->id());
+    client->ping();
+  } else if(type == WS_EVT_DISCONNECT){
+    Serial.printf("ws[%s][%u] disconnect\n", server->url(), client->id());
+  } else if(type == WS_EVT_ERROR){
+    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+  } else if(type == WS_EVT_PONG){
+    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+  } else if(type == WS_EVT_DATA){
+    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    String msg = "";
+    if(info->final && info->index == 0 && info->len == len){
+      //the whole message is in a single frame and we got all of it's data
+      //Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+
+      if(info->opcode == WS_TEXT){
+        for(size_t i=0; i < info->len; i++) {
+          msg += (char) data[i];
+        }
+      } else {
+        char buff[3];
+        for(size_t i=0; i < info->len; i++) {
+          sprintf(buff, "%02x ", (uint8_t) data[i]);
+          msg += buff ;
+        }
+      }
+      Serial.println(msg.c_str());
+      if(info->opcode == WS_TEXT)
+        wSet.textAll(msg.c_str());
+        wSave.cleanupClients();
+        wSet.cleanupClients();
+    } 
+  }
+}
+
+void onSave(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
   if(type == WS_EVT_CONNECT){
     Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
     client->printf("Hello Client %u :)", client->id());
@@ -49,54 +87,11 @@ void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventT
         }
       }
       Serial.println(msg.c_str());
-
       if(info->opcode == WS_TEXT)
-        client->text("I got your text message");
-      else
-        client->binary("I got your binary message");
+        client->text("{status: \"true\"}");
+        wSet.cleanupClients();
+        wSave.cleanupClients();
     } 
-  }
-}
-
-bool loadConfiguration()
-{
-  bool success = false;
-  //read configuration from FS json
-  if (SPIFFS.begin())
-  {
-    if (SPIFFS.exists("/config.json"))
-    {
-      //file exists, reading and loading
-      File configFile = SPIFFS.open("/config.json", "r");
-      if (configFile)
-      {
-        size_t size = configFile.size();
-        // Allocate a buffer to store contents of the file.
-        std::unique_ptr<char[]> buf(new char[size]);
-
-        configFile.readBytes(buf.get(), size);
-        DynamicJsonDocument doc(size);
-        DeserializationError error = deserializeJson(doc, configFile);
-        if (error) {
-          Serial.print(F("deserializeJson() failed: "));
-          Serial.println(error.c_str());
-          return false;
-        }
-      }
-    }
-  }
-  return success;
-}
-
-void setBoolean(char *&payload, bool &state)
-{
-  if (strcmp(payload, (char *)"ON") == 0 || strcmp(payload, (char *)"TRUE") == 0 || strcmp(payload, (char *)"true") == 0)
-  {
-    state = true;
-  }
-  else
-  {
-    state = false;
   }
 }
 
@@ -125,19 +120,15 @@ void setup()
 {
   Serial.begin(115200);
   connect();
-  ws.onEvent(onWsEvent);
-  server.addHandler(&ws);
-
-  events.onConnect([](AsyncEventSourceClient *client){
-    client->send("hello!",NULL,millis(),1000);
-  });
-  server.addHandler(&events);
+  wSet.onEvent(onSet);
+  wSave.onEvent(onSave);
+  server.addHandler(&wSet);
+  server.addHandler(&wSave);
   server.begin();
 }
 
 void loop()
 {
-  ws.cleanupClients();
 }
 
 void log(const char *message)
